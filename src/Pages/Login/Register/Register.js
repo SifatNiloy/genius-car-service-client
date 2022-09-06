a7fd0e3ebfe43d1a4e3c70a60858d4b86489161e
unknown <sifatniloy18@gmail.com>
2022-09-06 01:56:14 -0600

    google and github sign in button added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     const [
@@ -36,7 +37,9 @@ const Register = () => {
                 <input type="submit" value="Register" />
             </form>
             <p>Already have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div >
+
     );
 };
 
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,6 +1,18 @@
 import React from 'react';
-
+import google from '../../../images/social/google.png'
+import github from '../../../images/social/github.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (error) {
+        return (
+            <div>
+                <p className='text-danger'>Error: {error.message}</p>
+            </div>
+        );
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -9,7 +21,16 @@ const SocialLogin = () => {
                 <div style={{ height: '1px' }} className='bg-primary w-50'> </div>
             </div>
             <div>
-                <button className='btn btn-primary w-50'>Google Sign In</button>
+                <button className='btn btn-info d-block w-50 mx-auto'>
+                    <img className='me-2' style={{ width: '24px' }} src={google} alt="" />
+                    Google Sign In
+                </button>
+            </div>
+            <div>
+                <button className='btn btn-info d-block w-50 mx-auto my-2'>
+                    <img className='me-2' style={{ width: '24px' }} src={github} alt="" />
+                    Github Sign In
+                </button>
             </div>
         </div>
     );
