61fedfb243d6da91ca1eb4ecd40dbc1472f2880c
unknown <sifatniloy18@gmail.com>
2022-10-19 13:09:19 -0600

    checkout page form added

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
 
 
 const Checkout = () => {
+    const { serviceId } = useParams();
+    const [service] = useServiceDetail(serviceId);
     return (
-        <div>
-            <h2>please checkout your booking</h2>
-
+        <div className='w-50 mx-auto'>
+            <h2>please order: {service.name} </h2>
+            <form>
+                <input className='w-100 mb-2' type="text" name='name' placeholder='name' required /><br />
+                <input className='w-100 mb-2' type="text" name='email' placeholder='email' required /><br />
+                <input className='w-100 mb-2' value={service.name} type="text" name='service' placeholder='service' required /><br />
+                <input className='w-100 mb-2' type="text" name='address' placeholder='address' required /><br />
+                <input className='w-100 mb-2' type="text" name='phone' placeholder='phone' required /><br />
+                <input className='btn btn-primary' type="submit" value="place order" />
+            </form>
         </div>
     );
 };
