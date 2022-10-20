a8b51cf9ed29b9e37f6e1ac8b8b38b1d3288a39c
unknown <sifatniloy18@gmail.com>
2022-10-19 21:44:23 -0600

    adding access token

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import { toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
@@ -31,15 +32,18 @@ const Login = () => {
     if (loading || sending) {
         return <Loading></Loading>
     }
-    if (user) {
-        navigate(from, { replace: true });
-    }
-    const handleSubmit = event => {
+    // if (user) {
+    //     // navigate(from, { replace: true });
+    // }
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
     const navigateRegister = event => {
         navigate('/register')
